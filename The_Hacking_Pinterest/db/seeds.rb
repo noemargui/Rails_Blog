2203aeb56f74848c7fe298892b6397798c4a84d3
Noé Marguillard <noe.marguillard@essec.edu>
2019-01-30 18:36:48 +0100

    Pinterest Added

--- The_Hacking_Pinterest/db/seeds.rb
+++ The_Hacking_Pinterest/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |user|
+	User.create(name: Faker::Name.unique.first_name)
+end
+
+20.times do |pin|
+	Pin.create(url: Faker::Internet.unique.domain_name, user_id: (rand(User.first.id..User.last.id)))
+end
+
+40.times do |comment|
+	Comment.create(content: Faker::GreekPhilosophers.unique.quote, pin_id: (rand(Pin.first.id..Pin.last.id)), user_id: (rand(User.first.id..User.last.id)))
+end
